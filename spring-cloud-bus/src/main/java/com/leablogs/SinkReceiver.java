0fb21edf49c2977ef3974cded2f140a6d7fe56e3
18932900682@189.cn <shilh123@live.com>
2022-03-08 17:10:19 +0800

    spring cloud bus spring cloud config

---
+++ spring-cloud-bus/src/main/java/com/leablogs/SinkReceiver.java
@@ -0,0 +1,13 @@
+package com.leablogs;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+public class SinkReceiver {
+	@StreamListener(Sink.INPUT)
+	public void receive(Object payload) {
+		System.out.println("Received: " + payload);
+	}
+}
---
+++ spring-cloud-bus/src/main/java/com/leablogs/SpringCloudBus.java
@@ -0,0 +1,12 @@
+package com.leablogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBus {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringCloudBus.class, args);
+	}
+
+}
