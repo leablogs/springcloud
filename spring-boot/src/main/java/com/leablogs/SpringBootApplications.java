015d5e659aa479a97018dce3b0f771fadebadd34
shilh <shilh123@live.com>
2022-12-22 22:17:14 +0800

    同步代码

--- spring-boot/src/main/java/com/leablogs/SpringBootApplications.java
+++ spring-boot/src/main/java/com/leablogs/SpringBootApplications.java
@@ -1,7 +1,6 @@
 package com.leablogs;
 
 import lombok.extern.log4j.Log4j;
-import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.MDC;
 import org.springframework.boot.Banner;
@@ -14,15 +13,16 @@ import java.util.UUID;
 @SpringBootApplication
 @EnableScheduling
 @MapperScan(value = "com.leablogs.user.mapper")
-@Log4j2
+@Log4j
 public class SpringBootApplications {
     public static void main(String[] args) {
-//        System.setProperty("spring.config.name", "application"); // 设置指定加载的配置文件
-//        SpringApplication application = new SpringApplication(SpringBootApplications.class);
-//        application.setBannerMode(Banner.Mode.OFF);
-//        application.run(args);
+        log.info("22222222222222222222");
+        MDC.put("traceId", UUID.randomUUID().toString());
+        System.setProperty("spring.config.name", "application"); // 设置指定加载的配置文件
+        SpringApplication application = new SpringApplication(SpringBootApplications.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
         // 等同与以上三行
-//        MDC.put("traceId", UUID.randomUUID().toString());
-        SpringApplication.run(SpringBootApplications.class, args);
+        // SpringApplication.run(SpringBootApplications.class, args);
     }
 }
